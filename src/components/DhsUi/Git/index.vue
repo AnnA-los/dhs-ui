29d591c7c1185c35cd601ff8fa24f0249e1a0eef
Anna-los <1027218596@qq.com>
2026-06-14 19:44:02 +0800

    初始化 dhs-ui 项目

---
+++ src/components/DhsUi/Git/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DhsUiGit',
+  data() {
+    return {
+      url: ''
+    }
+  },
+  methods: {
+    goto() {
+      if (this.url) {
+        window.open(this.url)
+      }
+    }
+  }
+}
+</script>
